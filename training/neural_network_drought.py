1ded7863629402d593893f8716d9d105688c9668
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2025-03-20 21:16:00 +0100

    training drought

--- training/neural_network_drought.py
+++ training/neural_network_drought.py
@@ -5,26 +5,26 @@ import torch.nn.functional as F
 
 
 class NN_drought(nn.Module):
-    def __init__(self, input_size, output_size):
+    def __init__(self, input_size, output_size, device):
         '''Creates the model'''
         super(NN_drought, self).__init__()
-        self.fc1 = nn.Linear(input_size, 256)
-        self.ac1 = nn.SiLU()
-        self.fc2 = nn.Linear(256, 512)
-        self.ac2 = nn.SiLU()
-        self.fc3 = nn.Linear(512, 256)
-        self.ac3 = nn.SiLU()
-        self.fc4 = nn.Linear(256, output_size)
+        self.fc1 = nn.Linear(input_size, 128)
+        self.ac1 = nn.ReLU()
+        self.fc2 = nn.Linear(128, 128)
+        self.ac2 = nn.ReLU()
+        self.fc3 = nn.Linear(128, output_size)
+        self.ac3 = nn.Sigmoid()
+        self.device = device
 
     def forward(self, state):
         '''Do the prediction given the input'''
+        state.to(self.device)
         x = self.fc1(state)
         x = self.ac1(x)
         x = self.fc2(x)
         x = self.ac2(x)
         x = self.fc3(x)
         x = self.ac3(x)
-        x = self.fc4(x)
         return x
     
     def save(self, path):
