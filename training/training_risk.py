7dffa51a06ee6f42423e8218c4e60e4fc29741e2
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2025-03-20 19:26:03 +0100

    training and test done

--- training/training_risk.py
+++ training/training_risk.py
@@ -17,7 +17,9 @@ res = list(mda.get_meteobluedata_with_risk_numpy('./stress_buster_data.db', 'Soy
 historical = []
 forecast = []
 prediction = []
-for i in range(len(res)):
+indexes = np.random.permutation(len(res))
+res = [res[i] for i in indexes]
+for i in range(int(0.7*len(res))):
     historical.append(res[i][0])
     forecast.append(res[i][1])
     prediction.append(res[i][2]/9)
@@ -48,14 +50,38 @@ for epoch in range(epochs):
         y = y.reshape((batch_size, 36))
 
         y_predicted = neural_network(x)
-        epoch_loss += criterion(y_predicted, y)
-        #loss.backward()
-        #optimizer.step()
+        loss = criterion(y_predicted, y)
+        loss.backward()
+        optimizer.step()
         num_batches += 1
-        #epoch_loss += loss.item()
-    avg_loss = epoch_loss / num_batches  
-    avg_loss.backward()
-    optimizer.step()
+        epoch_loss += loss.item()
     epoch_loss /= num_batches
     if epoch % 10 == 0:
         print(f"Epochs:{epoch}, loss: {epoch_loss}")
+
+test_historical = []
+test_forecast = []
+test_prediction = []
+for i in range(int(0.7*len(res)), len(res)):
+    test_historical.append(res[i][0])
+    test_forecast.append(res[i][1])
+    test_prediction.append(res[i][2]/9)
+loss = 0
+for j in range(0, len(test_historical)):
+    x = test_historical[j]
+    f = test_forecast[j]
+    y = test_prediction[j]
+
+    x = torch.tensor(np.array(x), dtype=torch.float32).to(device)
+    f = torch.tensor(np.array(f), dtype=torch.float32).to(device)
+    y = torch.tensor(np.array(y), dtype=torch.float32).to(device)
+    x = x.reshape((1, -1))
+    f = f.reshape((1, -1))
+    x = torch.cat((x, f), 1).to(device)
+    y = y.reshape((1, 36))
+
+    y_predicted = neural_network(x)
+    loss += criterion(y_predicted, y)
+print("Average loss: ", loss/len(test_historical))
+
+neural_network.save('risk_model.pth')
