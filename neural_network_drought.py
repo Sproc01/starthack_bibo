89465da3820c5eeef41383a7d03b4a64aa1d34e4
Sproc01 <95143387+Sproc01@users.noreply.github.com>
2025-03-20 11:04:39 +0100

    nets blue print

---
+++ neural_network_drought.py
@@ -0,0 +1,56 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class NN_drought(nn.Module):
+    def __init__(self, input_size, output_size):
+        '''Creates the model'''
+        super(NN_drought, self).__init__()
+        self.fc1 = nn.Linear(input_size, 256)
+        self.ac1 = nn.SiLU()
+        self.fc2 = nn.Linear(256, 512)
+        self.ac2 = nn.SiLU()
+        self.fc3 = nn.Linear(512, 256)
+        self.ac3 = nn.SiLU()
+        self.fc4 = nn.Linear(256, output_size)
+
+    def forward(self, state):
+        '''Do the prediction given the input'''
+        x = self.fc1(state)
+        x = self.ac1(x)
+        x = self.fc2(x)
+        x = self.ac2(x)
+        x = self.fc3(x)
+        x = self.ac3(x)
+        x = self.fc4(x)
+        return x
+    
+    def save(self, path):
+        '''Save the model'''
+        torch.save(self.state_dict(), path)
+    
+    def load(self, path):
+        torch.load_state_dict(torch.load(path))
+
+
+neural_network = NN_drought(1460, 3*12)
+epochs = 10
+learning_rate = 0.001
+optimizer = torch.optim.Adam(neural_network.parameters(), lr=learning_rate)
+criterion = nn.MSELoss()
+
+for i in range(epochs):
+    dataset = np.load('dataset.npy')
+    X = dataset[:, :-3*12]
+    y = dataset[:, -3*12:]
+    optimizer.zero_grad()
+    X = torch.tensor(X, dtype=torch.float32)
+    y = torch.tensor(y, dtype=torch.float32)
+    y_predicted = neural_network(X)
+    loss = criterion(y_predicted, y)
+    if i % 100 == 0:
+        print(f"Epochs:{i}, loss: {loss}")
+    loss.backward()
+    optimizer.step()
